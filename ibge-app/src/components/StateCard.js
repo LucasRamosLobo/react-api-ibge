742d0200677ac4505e32a7e9e8f5fdd7979b9542
Lucas Ramos <lucasramoslobo83@gmail.com>
2023-04-12 13:17:45 -0300

    build

--- ibge-app/src/components/StateCard.js
+++ ibge-app/src/components/StateCard.js
@@ -1,17 +1,71 @@
-import React from 'react';
-import { Card } from 'react-bootstrap';
+import React, { useState, useEffect } from 'react';
+import { Container, Card, Row, Col, Form } from 'react-bootstrap';
 
 function StateCard({ state }) {
+  const [municipios, setMunicipios] = useState([]);
+  const [selectedMunicipio, setSelectedMunicipio] = useState(null);
+  const [area, setArea] = useState(null);
+
+  useEffect(() => {
+    async function loadMunicipios() {
+      const response = await fetch(`https://servicodados.ibge.gov.br/api/v1/localidades/estados/${state.id}/municipios`);
+      const data = await response.json();
+      setMunicipios(data);
+    }
+
+    loadMunicipios();
+  }, [state]);
+
+  async function handleSelectMunicipio(event) {
+    const selectedMunicipioNome = event.target.value;
+    const selectedMunicipio = municipios.find(municipio => municipio.nome === selectedMunicipioNome);
+    setSelectedMunicipio(selectedMunicipio);
+    const response = await fetch(`https://servicodados.ibge.gov.br/api/v3/malhas/municipios/${selectedMunicipio.id}/metadados`);
+    const data = await response.json();
+    setArea(data[0].area.dimensao);
+  }
+
   return (
-    <Card style={{ width: '18rem' }}>
-      <Card.Body>
-        <Card.Title>{state.nome}</Card.Title>
-        <Card.Subtitle className="mb-2 text-muted">{state.sigla}</Card.Subtitle>
-        <Card.Text>
-          <p>Área: {state.regiao.nome}</p>
-        </Card.Text>
-      </Card.Body>
-    </Card>
+    <Container className="mt-3">
+      <Card className="mb-3">
+        <Card.Body>
+          <Row>
+            <Col>
+              <h2 className="text-center">{state.nome}</h2>
+              <p>Sigla: {state.sigla}</p>
+            </Col>
+          </Row>
+        </Card.Body>
+      </Card>
+
+        <Card.Body>
+          <Form>
+            <Form.Group controlId="formStateSelect">
+              <Form.Label>Município</Form.Label>
+              <Form.Control as="select" value={selectedMunicipio ? selectedMunicipio.nome : ''} onChange={handleSelectMunicipio}>
+                <option value="">Selecione um município</option>
+                {municipios.map(municipio => (
+                  <option key={municipio.id} value={municipio.nome}>{municipio.nome}</option>
+                ))}
+              </Form.Control>
+            </Form.Group>
+          </Form>
+          {selectedMunicipio && (
+            <div>
+              <hr />
+              <Row>
+                <Col md={6} sm={12}>
+                  <p className="text-center">Microregião: {selectedMunicipio.microrregiao.nome}</p>
+                </Col>
+                <Col md={6} sm={12}>
+                  <p className="text-center">área: {area}</p>
+                </Col>
+              </Row>
+            </div>
+          )}
+        </Card.Body>
+ 
+    </Container>
   );
 }
 
--- ibge-app/src/components/StateList.js
+++ ibge-app/src/components/StateList.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import StateCard from './StateCard';
-import { Container, Form } from 'react-bootstrap';
+import { Container, Form, Card } from 'react-bootstrap';
 
 function StateList() {
   const [states, setStates] = useState([]);
@@ -23,23 +23,25 @@ function StateList() {
   const selectedStateData = states.find(state => state.nome === selectedState);
 
   return (
-    <Container>
-      <h1>Selecione um estado:</h1>
-      <Form>
-        <Form.Group controlId="formStateSelect">
-          <Form.Label>Estado</Form.Label>
-          <Form.Control as="select" value={selectedState} onChange={handleSelectState}>
-            <option value="">Selecione um estado</option>
-            {states.map(state => (
-              <option key={state.id} value={state.nome}>{state.nome}</option>
-            ))}
-          </Form.Control>
-        </Form.Group>
-      </Form>
-      {selectedStateData && (
-        <StateCard state={selectedStateData} />
-      )}
-    </Container>
+      <Container>
+         <Card>
+        <h1>Selecione um estado:</h1>
+        <Form>
+          <Form.Group controlId="formStateSelect">
+            <Form.Label>Estado</Form.Label>
+            <Form.Control as="select" value={selectedState} onChange={handleSelectState}>
+              <option value="">Selecione um estado</option>
+              {states.map(state => (
+                <option key={state.id} value={state.nome}>{state.nome}</option>
+              ))}
+            </Form.Control>
+          </Form.Group>
+        </Form>
+        {selectedStateData && (
+          <StateCard state={selectedStateData} />
+        )}
+            </Card>
+      </Container>
   );
 }
 
